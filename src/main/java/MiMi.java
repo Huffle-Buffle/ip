06128e338d27f17b534ace8716686d010e5efb6a
megane <meganekishatu22@gmail.com>
2025-08-18 11:09:22 +0800

    Add MiMi.java

--- src/main/java/MiMi.java
+++ src/main/java/MiMi.java
@@ -1,4 +1,4 @@
-public class Duke {
+public class MiMi {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
