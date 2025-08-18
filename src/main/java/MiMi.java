55c5380b6e6add16674ba414d9eeb4ca529f90dd
megane <meganekishatu22@gmail.com>
2025-08-18 11:50:09 +0800

    Did level 2, now MiMi can add to a list

--- src/main/java/MiMi.java
+++ src/main/java/MiMi.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MiMi {
     private static final String LINE = "____________________________________________________________";
@@ -12,22 +14,34 @@ public class MiMi {
 
     private static void byebye() {
         System.out.println(LINE);
-        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println("     Bye. Hope to see you again soon!");
         System.out.println(LINE);
     }
 
     public static void main(String[] args) {
+        List<String> tasks = new ArrayList<>();
+
         sayhi();
         Scanner sc = new Scanner(System.in);
         while (sc.hasNextLine()) {
             String input = sc.nextLine();
-            if (input.trim().equals("bye")) {
+            String cmd = input.trim();
+
+            if (cmd.equals("bye")) {
                 byebye();
                 break;
+            } else if (cmd.equals("list")) {
+                System.out.println(LINE);
+                for (int i = 0; i < tasks.size(); i++) {
+                    System.out.println("      " + (i + 1) + ". " + tasks.get(i));
+                }
+                System.out.println(LINE);
+            } else {
+                tasks.add(input);
+                System.out.println(LINE);
+                System.out.println("        added: " + input);
+                System.out.println(LINE);
             }
-            System.out.println(LINE);
-            System.out.println("       " + input);
-            System.out.println(LINE);
         }
         sc.close();
     }
