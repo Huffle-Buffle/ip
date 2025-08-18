09c5acdf7c1b8c1d44a54fb57c5db32a0415d266
megane <meganekishatu22@gmail.com>
2025-08-18 11:34:55 +0800

    did level 1 task of Echo

--- src/main/java/MiMi.java
+++ src/main/java/MiMi.java
@@ -1,11 +1,34 @@
+import java.util.Scanner;
+
 public class MiMi {
-    public static void main(String[] args) {
-        String line = "____________________________________________________________";
-        System.out.println(line);
+    private static final String LINE = "____________________________________________________________";
+
+    private static void sayhi() {
+        System.out.println(LINE);
         System.out.println(" Hello! I'm MiMi");
         System.out.println(" What can I do for you?");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
+
+    private static void byebye() {
+        System.out.println(LINE);
         System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println(line);
+        System.out.println(LINE);
+    }
+
+    public static void main(String[] args) {
+        sayhi();
+        Scanner sc = new Scanner(System.in);
+        while (sc.hasNextLine()) {
+            String input = sc.nextLine();
+            if (input.trim().equals("bye")) {
+                byebye();
+                break;
+            }
+            System.out.println(LINE);
+            System.out.println("       " + input);
+            System.out.println(LINE);
+        }
+        sc.close();
     }
 }
