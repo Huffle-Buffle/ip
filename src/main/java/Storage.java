a225ec17528e1bbfc94d609fb28d61fc13169f34
megane <meganekishatu22@gmail.com>
2025-08-28 15:31:48 +0800

    feat(storage): extends Save class

--- src/main/java/Storage.java
+++ src/main/java/Storage.java
@@ -1,4 +1,5 @@
-package PACKAGE_NAME;
-
-public class Storage {
+public class Storage extends Save {
+    public Storage(String filePath) {
+        super(); // Save() already sets up data/MiMi.txt and creates folders/files
+    }
 }
